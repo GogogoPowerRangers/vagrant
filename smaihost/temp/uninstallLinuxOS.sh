cb0240a2af2ec1b32dc8efc112093457796cf69b
Dean Okamura <dokamura@us.ibm.com>
2013-10-02 12:32:12 -0700

    lunch

---
+++ smaihost/temp/uninstallLinuxOS.sh
@@ -0,0 +1,4 @@
+sudo yum remove smai-framework-jre-07.04.02.00-1.el6.x86_64
+sudo yum remove smai-os-06.30.02.00-1.el6.x86_64
+sudo yum remove smai-itm-os-06.35.00.00-1.el6.x86_64
+sudo yum remove smai-framework-core-06.35.00.00-1.el6.x86_64
