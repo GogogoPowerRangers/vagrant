dc6a5dab3d95a5eec1b6583697f3de2954bd2ed4
Dean Okamura <dokamura@us.ibm.com>
2013-10-03 17:26:45 -0700

    uninstall

--- smaihost/temp/uninstallLinuxOS.sh
+++ smaihost/temp/uninstallLinuxOS.sh
@@ -1,4 +1,18 @@
-sudo yum remove smai-framework-jre-07.04.02.00-1.el6.x86_64
-sudo yum remove smai-os-06.30.02.00-1.el6.x86_64
-sudo yum remove smai-itm-os-06.35.00.00-1.el6.x86_64
-sudo yum remove smai-framework-core-06.35.00.00-1.el6.x86_64
+#!/bin/bash
+#
+# Licensed Materials - Property of IBM
+#
+# 2013-IBM
+#
+# (C) Copyright IBM Corp. 2013  All Rights Reserved.
+#
+# US Government Users Restricted Rights - Use, duplication or
+# disclosure restricted by GSA ADP Schedule Contract with
+# IBM Corp.
+#
+
+sudo yum remove -y smai-os-06.30.02.00-1.el6.x86_64
+sudo yum remove -y smai-framework-core-06.35.00.00-1.el6.x86_64
+sudo yum remove -y smai-framework-jre-07.04.02.00-1.el6.x86_64
+
+#
