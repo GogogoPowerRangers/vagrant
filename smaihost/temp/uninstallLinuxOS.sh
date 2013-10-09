c753ca9c1d26567570217987677eb963b406ea20
Dean Okamura <dokamura@us.ibm.com>
2013-10-09 09:15:58 -0700

    uninstall

--- smaihost/temp/uninstallLinuxOS.sh
+++ smaihost/temp/uninstallLinuxOS.sh
@@ -11,8 +11,26 @@
 # IBM Corp.
 #
 
+sudo killall -KILL klzagent
+sudo kill -KILL kcawd
+
+if [ -d /tmp/smai-fabric ] ; then
+    cd /tmp/smai-fabric
+    sudo ./uninstall.sh
+    cd /tmp
+fi
+
 sudo yum remove -y smai-os-06.30.02.00-1.el6.x86_64
 sudo yum remove -y smai-framework-core-06.35.00.00-1.el6.x86_64
 sudo yum remove -y smai-framework-jre-07.04.02.00-1.el6.x86_64
+
+sudo rm -rf /opt/ibm/ccm/agent
+sudo rm -rf /opt/IBM/*
+
+sudo rm -rf /opt/ibm/ccm
+sudo rm -rf /opt/ibm/gaian
+sudo rm -rf /opt/ibm/wlp
+
+sudo rm -rf /tmp/smai-*
 
 #
