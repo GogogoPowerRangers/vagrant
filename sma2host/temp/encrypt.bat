1476220c7235b83b7377df8f626ea61fd98a8435
Dean Okamura <dokamura@us.ibm.com>
2013-10-11 19:43:35 -0700

    tied

---
+++ sma2host/temp/encrypt.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+@REM
+@REM Copyright 2013 Dean Okamura
+@REM
+@REM Licensed under the Apache License, Version 2.0 (the "License");
+@REM you may not use this file except in compliance with the License.
+@REM You may obtain a copy of the License at
+@REM
+@REM     http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing, software
+@REM distributed under the License is distributed on an "AS IS" BASIS,
+@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM See the License for the specific language governing permissions and
+@REM limitations under the License.
+@REM
+
+@setlocal
+
+cd C:\IBM\ITM\CAS\BIN
+if NOT EXIST kglcryut.exe copy Z:\itmdev\sdk\wv9x64\dst\bin\kglcryut.exe
+
+set KEYFILE_DIR=C:\IBM\ITM\keyfiles
+set KDEBE_KEYRING_FILE=C:\IBM\ITM\keyfiles\keyfile.kdb
+set KDEBE_KEYRING_STASH=C:\IBM\ITM\keyfiles\keyfile.sth
+
+kglcryut Encrypt "passw0rd"
+
+:done
+@endlocal
+:EOF
