a42881809ba3ecc287964892aadfc987f20faaa8
Dean Okamura <dokamura@us.ibm.com>
2013-11-05 13:41:44 -0800

    endpoint

---
+++ endhost/chef/chef.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Copyright 2013 Dean Okamura
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# file_cache_path
+if [ ! -d /tmp/vagrant-chef-1/chef-solo-1 ] ; then
+    mkdir -p /tmp/vagrant-chef-1/chef-solo-1
+fi
+# cookbook_path
+if [ ! -d /tmp/vagrant-chef-1/chef-solo-1/cookbooks -a \
+     ! -L /tmp/vagrant-chef-1/chef-solo-1/cookbooks -a \
+     -d $HOME/apmaas/endpoint/cookbooks ] ; then
+   ln -sf $HOME/cookbooks /tmp/vagrant-chef-1/chef-solo-1/cookbooks
+fi
+if [ ! -d /tmp/vagrant-chef-1/chef-solo-2/cookbooks -a \
+     ! -L /tmp/vagrant-chef-1/chef-solo-1/cookbooks -a \
+     -d $HOME/cookbooks ] ; then
+   ln -sf $HOME/cookbooks /tmp/vagrant-chef-1/chef-solo-2/cookbooks
+fi
+
+# Vagrant usually runs Chef as root
+# Note: RECIPE is an experimental environment variable
+if [ ! "$1" = "" ] ; then
+    RECIPE=$1
+fi
+case "$RECIPE" in
+bare|dev ) sudo chef-solo -j $RECIPE.json -c solo.rb ;;
+* ) sudo chef-solo -j default.json -c solo.rb ;;
+esac
+
+#
---
+++ endhost/chef/solo.rb
@@ -0,0 +1,15 @@
+file_cache_path "/tmp/vagrant-chef-1"
+cookbook_path ["/tmp/vagrant-chef-1/chef-solo-1/cookbooks", "/tmp/vagrant-chef-1/chef-solo-2/cookbooks"]
+log_level :info
+
+encrypted_data_bag_secret "/tmp/encrypted_data_bag_secret"
+
+
+
+http_proxy nil
+http_proxy_user nil
+http_proxy_pass nil
+https_proxy nil
+https_proxy_user nil
+https_proxy_pass nil
+no_proxy nil
