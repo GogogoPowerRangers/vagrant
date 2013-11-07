9625339d3c71c5b57bbe5f00187e447b4aa76956
Dean Okamura <dokamura@us.ibm.com>
2013-11-06 19:25:00 -0800

    thin

--- endhost/chef/thin.sh
+++ endhost/chef/thin.sh
@@ -15,8 +15,9 @@
 # limitations under the License.
 #
 
-# Use ruby-tasks.sh if Chef running as root user does not execute rvm installed files.
+# Use thin.sh if thin running as service does not process HTTP requests.
 # Seems to be a problem with VM hosts.
+# Limitation: Requires login window and Ctrl-C termination.
 
 tasks()
 {
@@ -31,29 +32,7 @@ tasks()
         echo "$(pwd) should be /home/endpoint/saas_endpoint"
         return 1
     fi
-    if [ -f /home/endpoint/ruby-tasks.done ] ; then
-        echo "/home/endpoint/ruby-tasks.done exists"
-        echo "Delete /home/endpoint/ruby-tasks.done to run this script"
-        return 0
-    fi
-    gem install bundler
-    if [ $? -ne 0 ] ; then
-        echo "gem install bundler failed"
-        return 1
-    fi
-    bundle install
-    if [ $? -ne 0 ] ; then
-        echo "gem bundle install failed"
-        return 1
-    fi
-    rake db:migrate
-    if [ $? -ne 0 ] ; then
-        echo "gem rake db:migrate failed"
-        return 1
-    fi
-    touch /home/endpoint/ruby-tasks.done
-    chown endpoint:endpoint /home/endpoint/ruby-tasks.done
-    return 0
+    bundle exec thin start
 }
 
 case "$1" in
