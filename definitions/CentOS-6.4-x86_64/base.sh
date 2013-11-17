ac3b50a869c6c14eb831555987df6839fb15dc04
Dean Okamura <dokamura@aol.com>
2013-11-17 00:16:45 -0800

    updates

--- definitions/CentOS-6.4-x86_64/base.sh
+++ definitions/CentOS-6.4-x86_64/base.sh
@@ -2,14 +2,6 @@
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
-cat > /etc/yum.repos.d/epel.repo << EOM
-[epel]
-name=epel
-baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
-enabled=1
-gpgcheck=0
-EOM
-
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
 
 # APM as a service
@@ -18,11 +10,9 @@ yum install -y unzip rsync ruby-devel zip
 yum install -y sqlite-devel java-1.7.0-openjdk
 
 # EPEL repository for nginx and node.js
-mkdir /home/vagrant/temp
-cd /home/vagrant/temp
-wget http://mirrors.cat.pdx.edu/epel/6/i386/epel-release-6-8.noarch.rpm
-sudo yum install -y epel-release-6-8.noarch.rpm 
-sudo yum install nginx nodejs
+wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+rpm -ivh epel-release-6-8.noarch.rpm
+sudo yum install -y nginx nodejs
 
 # Make ssh faster by not waiting on DNS
 echo "UseDNS no" >> /etc/ssh/sshd_config
