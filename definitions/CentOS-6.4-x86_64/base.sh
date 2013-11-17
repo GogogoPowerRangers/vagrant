09ce286eb3323ebc26fee524814864743046b040
Dean Okamura <dokamura@us.ibm.com>
2013-11-16 20:34:30 -0800

    hack

--- definitions/CentOS-6.4-x86_64/base.sh
+++ definitions/CentOS-6.4-x86_64/base.sh
@@ -12,5 +12,17 @@ EOM
 
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
 
+# APM as a service
+yum groupinstall -y "Development Tools"
+yum install -y unzip rsync ruby-devel zip
+yum install -y sqlite-devel java-1.7.0-openjdk
+
+# EPEL repository for nginx and node.js
+mkdir /home/vagrant/temp
+cd /home/vagrant/temp
+wget http://mirrors.cat.pdx.edu/epel/6/i386/epel-release-6-8.noarch.rpm
+sudo yum install -y epel-release-6-8.noarch.rpm 
+sudo yum install nginx nodejs
+
 # Make ssh faster by not waiting on DNS
 echo "UseDNS no" >> /etc/ssh/sshd_config
